33f989b9b451d9b863979b0f0de58823b8591240
Silvio Angelo <silvioangels@gmail.com>
2018-03-13 15:16:01 -0300

    Alteração para chave composta ID - nome funcional
    Alteração para chave composta ID - nome funcional

--- HbmChanges/src/main/java/br/com/vivere/app/ValidationsXmlTags.java
+++ HbmChanges/src/main/java/br/com/vivere/app/ValidationsXmlTags.java
@@ -57,7 +57,7 @@ public class ValidationsXmlTags {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(arquivoHbm);
-			doc.getDocumentElement().normalize();
+			doc.getDocumentElement().normalize(); 
 			
 			NodeList metaTestList = doc.getElementsByTagName("meta");
 			
@@ -66,180 +66,206 @@ public class ValidationsXmlTags {
 				continue;
 			}
 			
-			//Modifica
 			NodeList elementList = doc.getElementsByTagName("class");
-			Node classElement =  elementList.item(0);
+			Node classElement 	 =  elementList.item(0);
 			
-			//Tag Audit
-			NodeList commentList = doc.getElementsByTagName("comment");
-			Node commentElement =  commentList.item(0);
-			Element metaNode = doc.createElement("meta");
-			metaNode.setTextContent("com.viverebrasil.contextaware.audittracking.IAuditable");
-			Attr attAttribute = doc.createAttribute("attribute");
-			attAttribute.setValue("implements");
-			metaNode.setAttributeNode(attAttribute);
-			Attr attInherit = doc.createAttribute("inherit");
-			attInherit.setValue("false");
-			metaNode.setAttributeNode(attInherit);
-			classElement.insertBefore(metaNode, commentElement);
+			inserirTagAuditoria(doc, classElement);
 			
-			//Metas de use no id
-			NodeList idList = doc.getElementsByTagName("id");
-			if(idList != null && idList.getLength() > 0) {
-				Node idElement =  idList.item(0);
-				NodeList childNodeList = idElement.getChildNodes();
-				
-				//Meta
-				for (int i = 0; i < childNodeList.getLength(); i++) {
-					Node childElement =  childNodeList.item(i);
-					
-					if(childElement.getNodeName().equals("column")) {
-						
-						Element metaIdNode = doc.createElement("meta_alterar");
-						metaIdNode.setTextContent("true");
-						Attr attIdAttribute = doc.createAttribute("attribute");
-						attIdAttribute.setValue("use-in-tostring");
-						metaIdNode.setAttributeNode(attIdAttribute);
-						
-						Element metaId2Node = doc.createElement("meta_alterar");
-						metaId2Node.setTextContent("true");
-						Attr attId2Attribute = doc.createAttribute("attribute");
-						attId2Attribute.setValue("use-in-equals");
-						metaId2Node.setAttributeNode(attId2Attribute);
-						metaId2Node.removeAttribute("inherit");
-						
-						idElement.insertBefore(metaIdNode, childElement);
-						idElement.insertBefore(metaId2Node, childElement);
-						break;
-					}
-					
-				}
-				
-				NamedNodeMap attr = idElement.getAttributes();
-				Node attrType = attr.getNamedItem("type");
-				
-				if(attrType.getTextContent().equals("big_integer")) {
-					
-					for (int i = 0; i < childNodeList.getLength(); i++) {
-						
-						Node childElement =  childNodeList.item(i);
-						
-						if(childElement.getNodeName().equals("generator")) {
-							
-							Element generatorSequenceElement = doc.createElement("generator");
-							Attr attClass = doc.createAttribute("class");
-							attClass.setValue("org.hibernate.id.enhanced.SequenceStyleGenerator");
-							generatorSequenceElement.setAttributeNode(attClass);
-							
-							Element optimizerParamElement = doc.createElement("param");
-							optimizerParamElement.setTextContent("none");
-							Attr attOptName = doc.createAttribute("name");
-							attOptName.setValue("optimizer");
-							optimizerParamElement.setAttributeNode(attOptName);
-							generatorSequenceElement.appendChild(optimizerParamElement);
-							
-							Element incSizeParamElement = doc.createElement("param");
-							incSizeParamElement.setTextContent("1");
-							Attr attIncSizeName = doc.createAttribute("name");
-							attIncSizeName.setValue("increment_size");
-							incSizeParamElement.setAttributeNode(attIncSizeName);
-							generatorSequenceElement.appendChild(incSizeParamElement);
-							
-							NamedNodeMap attrClass = classElement.getAttributes();
-							Node attrTable = attrClass.getNamedItem("table");
-							Element seqNameParamElement = doc.createElement("param");
-							seqNameParamElement.setTextContent("SEQ_"+attrTable.getTextContent());
-							Attr seqNameSizeName = doc.createAttribute("name");
-							seqNameSizeName.setValue("sequence_name");
-							seqNameParamElement.setAttributeNode(seqNameSizeName);
-							generatorSequenceElement.appendChild(seqNameParamElement);
-							
-							idElement.replaceChild(generatorSequenceElement, childElement);
-							break;
-						}
-						
-					}
-					
-				}
-				
-			}
+			inserirTagsChaveSimples(doc, classElement);
 			
-			NodeList compositeIdList = doc.getElementsByTagName("composite-id");
-			if(idList != null && compositeIdList.getLength() > 0) {
-				
-				Node compositeIdElement =  compositeIdList.item(0);
-				NodeList childNodeList = compositeIdElement.getChildNodes();
-				Node keyPropertyElement =  childNodeList.item(0);
-				
-				Element metaIdCompNode = doc.createElement("meta");
-				metaIdCompNode.setTextContent("true");
-				Attr attIdCompAttribute = doc.createAttribute("attribute");
-				attIdCompAttribute.setValue("use-in-tostring");
-				metaIdCompNode.setAttributeNode(attIdCompAttribute);
-				
-				Element metaId2CompNode = doc.createElement("meta");
-				metaId2CompNode.setTextContent("true");
-				Attr attId2CompAttribute = doc.createAttribute("attribute");
-				attId2CompAttribute.setValue("use-in-equals");
-				metaId2CompNode.setAttributeNode(attId2CompAttribute);
-				
-				compositeIdElement.insertBefore(metaIdCompNode, keyPropertyElement);
-				compositeIdElement.insertBefore(metaId2CompNode, keyPropertyElement);
-			}
+			inserirTagsChaveComposta(doc, classElement, arquivoHbm);
 			
-			//Batimento dos tipos de serializable e float
-			NodeList propertyList = doc.getElementsByTagName("property");
+			validacaoTipagemPropriedades(doc);
 			
-			for (int i = 0; i < propertyList.getLength(); i++) {
-				
-				Node propertyElement =  propertyList.item(i);
-				
-				NamedNodeMap propertyMap = propertyElement.getAttributes();
-				Node attrType = propertyMap.getNamedItem("type");
-				
-				switch (TipoDados.recuperarEnum(attrType.getTextContent())) {
-					case SERIALIZABLE:
-						attrType.setTextContent(TipoDados.SERIALIZABLE.getNomeReal());
-						break;
-						
-					case FLOAT:
-						attrType.setTextContent(TipoDados.FLOAT.getNomeReal());
-						break;
-						
-					default:
-						break;
-				}
-				
-			}
+			validacaoNomesFuncionaisEntidades(doc, classElement,arquivoHbm);
 			
-			//Alterando os nomes funcionais
-			NamedNodeMap attrClass = classElement.getAttributes();
-			Node attrName = attrClass.getNamedItem("name");
-			String nomeTabelaHbm = arquivoHbm.getName().replace(".hbm.xml", "");
-			String nomeFuncional = attrName.getTextContent().replace(nomeTabelaHbm, "domain."+nomesFuncionaisMap.get(nomeTabelaHbm));
-			attrName.setTextContent(nomeFuncional);
-			
-			List<String>relationshipList = new ArrayList<String>(Arrays.asList("one-to-one","one-to-many","many-to-one","many-to-many")); 
-			
-			for (String relationship : relationshipList) {
-				alteracaoNomesFuncionaisRelacionamentosEntidades(doc, relationship);
-			}
-			
-			//Escreve
-			doc.getDocumentElement().normalize();
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(arquivoHbm);
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			transformer.transform(source, result);
-			System.out.println("Arquivo XML "+arquivoHbm.getName()+" atualizado com sucesso.");
+			escreverArquivoHBM(doc, classElement, arquivoHbm);
 			
 		}//fim da iteracao for
 		
 		System.out.println("Atualização de Todos HBMs realizado com sucesso.");
 		
 	}//fim do metodo main
+	
+	private static void inserirTagAuditoria(Document doc,Node classElement) {
+		
+		NodeList commentList = doc.getElementsByTagName("comment");
+		Node commentElement =  commentList.item(0);
+		Element metaNode = doc.createElement("meta");
+		metaNode.setTextContent("com.viverebrasil.contextaware.audittracking.IAuditable");
+		Attr attAttribute = doc.createAttribute("attribute");
+		attAttribute.setValue("implements");
+		metaNode.setAttributeNode(attAttribute);
+		Attr attInherit = doc.createAttribute("inherit");
+		attInherit.setValue("false");
+		metaNode.setAttributeNode(attInherit);
+		classElement.insertBefore(metaNode, commentElement);
+		
+	}//fim do metodo inserirTagAuditoria
+	
+	private static void inserirTagsChaveSimples(Document doc,Node classElement) {
+		
+		//Metas de use no id
+		NodeList idList = doc.getElementsByTagName("id");
+		if(idList != null && idList.getLength() > 0) {
+			Node idElement =  idList.item(0);
+			NodeList childNodeList = idElement.getChildNodes();
+			
+			//Meta
+			for (int i = 0; i < childNodeList.getLength(); i++) {
+				Node childElement =  childNodeList.item(i);
+				
+				if(childElement.getNodeName().equals("column")) {
+					
+					Element metaIdNode = doc.createElement("meta_alterar");
+					metaIdNode.setTextContent("true");
+					Attr attIdAttribute = doc.createAttribute("attribute");
+					attIdAttribute.setValue("use-in-tostring");
+					metaIdNode.setAttributeNode(attIdAttribute);
+					
+					Element metaId2Node = doc.createElement("meta_alterar");
+					metaId2Node.setTextContent("true");
+					Attr attId2Attribute = doc.createAttribute("attribute");
+					attId2Attribute.setValue("use-in-equals");
+					metaId2Node.setAttributeNode(attId2Attribute);
+					metaId2Node.removeAttribute("inherit");
+					
+					idElement.insertBefore(metaIdNode, childElement);
+					idElement.insertBefore(metaId2Node, childElement);
+					break;
+				}
+				
+			}
+			
+			NamedNodeMap attr = idElement.getAttributes();
+			Node attrType = attr.getNamedItem("type");
+			
+			if(attrType.getTextContent().equals("big_integer")) {
+				
+				for (int i = 0; i < childNodeList.getLength(); i++) {
+					
+					Node childElement =  childNodeList.item(i);
+					
+					if(childElement.getNodeName().equals("generator")) {
+						
+						Element generatorSequenceElement = doc.createElement("generator");
+						Attr attClass = doc.createAttribute("class");
+						attClass.setValue("org.hibernate.id.enhanced.SequenceStyleGenerator");
+						generatorSequenceElement.setAttributeNode(attClass);
+						
+						Element optimizerParamElement = doc.createElement("param");
+						optimizerParamElement.setTextContent("none");
+						Attr attOptName = doc.createAttribute("name");
+						attOptName.setValue("optimizer");
+						optimizerParamElement.setAttributeNode(attOptName);
+						generatorSequenceElement.appendChild(optimizerParamElement);
+						
+						Element incSizeParamElement = doc.createElement("param");
+						incSizeParamElement.setTextContent("1");
+						Attr attIncSizeName = doc.createAttribute("name");
+						attIncSizeName.setValue("increment_size");
+						incSizeParamElement.setAttributeNode(attIncSizeName);
+						generatorSequenceElement.appendChild(incSizeParamElement);
+						
+						NamedNodeMap attrClass = classElement.getAttributes();
+						Node attrTable = attrClass.getNamedItem("table");
+						Element seqNameParamElement = doc.createElement("param");
+						seqNameParamElement.setTextContent("SEQ_"+attrTable.getTextContent());
+						Attr seqNameSizeName = doc.createAttribute("name");
+						seqNameSizeName.setValue("sequence_name");
+						seqNameParamElement.setAttributeNode(seqNameSizeName);
+						generatorSequenceElement.appendChild(seqNameParamElement);
+						
+						idElement.replaceChild(generatorSequenceElement, childElement);
+						break;
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+	}//fim do metodo inserirTagsChaveSimples
+	
+	private static void inserirTagsChaveComposta(Document doc,Node classElement, File arquivoHbm) {
+		
+		NodeList compositeIdList = doc.getElementsByTagName("composite-id");
+		if(compositeIdList != null && compositeIdList.getLength() > 0) {
+			
+			Node compositeIdElement =  compositeIdList.item(0);
+			NodeList childNodeList = compositeIdElement.getChildNodes();
+			Node keyPropertyElement =  childNodeList.item(0);
+			
+			Element metaIdCompNode = doc.createElement("meta");
+			metaIdCompNode.setTextContent("true");
+			Attr attIdCompAttribute = doc.createAttribute("attribute");
+			attIdCompAttribute.setValue("use-in-tostring");
+			metaIdCompNode.setAttributeNode(attIdCompAttribute);
+			
+			Element metaId2CompNode = doc.createElement("meta");
+			metaId2CompNode.setTextContent("true");
+			Attr attId2CompAttribute = doc.createAttribute("attribute");
+			attId2CompAttribute.setValue("use-in-equals");
+			metaId2CompNode.setAttributeNode(attId2CompAttribute);
+			
+			compositeIdElement.insertBefore(metaIdCompNode, keyPropertyElement);
+			compositeIdElement.insertBefore(metaId2CompNode, keyPropertyElement);
+			
+			NamedNodeMap attrClass = compositeIdElement.getAttributes();
+			Node attrClassComp = attrClass.getNamedItem("class");
+			String nomeTabelaHbm = arquivoHbm.getName().replace(".hbm.xml", "");
+			String nomeTabelaHbmId = arquivoHbm.getName().replace(".hbm.xml", "").concat("Id");
+			String nomeFuncional = attrClassComp.getTextContent().replace(nomeTabelaHbmId, "domain."+nomesFuncionaisMap.get(nomeTabelaHbm).concat("Id"));
+			attrClassComp.setTextContent(nomeFuncional);
+		}
+		
+	}//fim do metodo inserirTagsChaveComposta
+	
+	private static void validacaoTipagemPropriedades(Document doc) {
+		
+		NodeList propertyList = doc.getElementsByTagName("property");
+		
+		for (int i = 0; i < propertyList.getLength(); i++) {
+			
+			Node propertyElement =  propertyList.item(i);
+			
+			NamedNodeMap propertyMap = propertyElement.getAttributes();
+			Node attrType = propertyMap.getNamedItem("type");
+			
+			switch (TipoDados.recuperarEnum(attrType.getTextContent())) {
+				case SERIALIZABLE:
+					attrType.setTextContent(TipoDados.SERIALIZABLE.getNomeReal());
+					break;
+					
+				case FLOAT:
+					attrType.setTextContent(TipoDados.FLOAT.getNomeReal());
+					break;
+					
+				default:
+					break;
+			}
+			
+		}
+		
+	}//fim do metodo validacaoTipagemPropriedades
+	
+	private static void validacaoNomesFuncionaisEntidades(Document doc,Node classElement, File arquivoHbm) {
+		
+		//Alterando os nomes funcionais
+		NamedNodeMap attrClass = classElement.getAttributes();
+		Node attrName = attrClass.getNamedItem("name");
+		String nomeTabelaHbm = arquivoHbm.getName().replace(".hbm.xml", "");
+		String nomeFuncional = attrName.getTextContent().replace(nomeTabelaHbm, "domain."+nomesFuncionaisMap.get(nomeTabelaHbm));
+		attrName.setTextContent(nomeFuncional);
+		
+		List<String>relationshipList = new ArrayList<String>(Arrays.asList("one-to-one","one-to-many","many-to-one","many-to-many")); 
+		
+		for (String relationship : relationshipList) {
+			alteracaoNomesFuncionaisRelacionamentosEntidades(doc, relationship);
+		}
+		
+	}//fim do metodo validacaoNomesFuncionaisEntidades
 	
 	private static void alteracaoNomesFuncionaisRelacionamentosEntidades(Document doc, String relationship) {
 		
@@ -255,6 +281,19 @@ public class ValidationsXmlTags {
 			String nomeFuncionaloneToMany = attrClas.getTextContent().replace(nomeTabelaHbmoneToMany, "domain."+nomesFuncionaisMap.get(nomeTabelaHbmoneToMany));
 			attrClas.setTextContent(nomeFuncionaloneToMany);
 		}
+		
+	}//fim do metodo alteracaoNomesFuncionaisRelacionamentosEntidades
+	
+	private static void escreverArquivoHBM(Document doc,Node classElement, File arquivoHbm) throws Exception {
+		
+		doc.getDocumentElement().normalize();
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(arquivoHbm);
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.transform(source, result);
+		System.out.println("Arquivo XML "+arquivoHbm.getName()+" atualizado com sucesso.");
 		
 	}
 
